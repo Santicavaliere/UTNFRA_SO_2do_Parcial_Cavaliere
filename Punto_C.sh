2faa6a652e4a6c2bebe678601a297a5416225ad2
Santicavaliere <santicavaliere@gmail.com>
2024-11-18 12:40:31 +0000

    Quinto commit, cambios en Punto_c

--- Punto_C.sh
+++ Punto_C.sh
@@ -1,30 +1,37 @@
-#!/bin/bash
 
-# Usé el cat <<EOF para crear el archivo index.html porque era más práctico que entrar a vim
-cat <<EOF > ~/UTN-FRA_SO_Examenes/202406/docker/index.html
-<div>
-<h1> Sistemas Operativos - UTNFRA </h1></br>
-<h2> 2do Parcial - Noviembre 2024 </h2> </br>
-<h3> Santiago Cavaliere</h3>
-<h3> División: 311</h3>
-</div>
-EOF
+cat index.html
 
-# Ruta completa al directorio donde se encuentra el Dockerfile
-dockerfile_path="/home/santicavaliere/UTN-FRA_SO_Examenes/202406/docker"
+sudo vim index.html
 
-# Construye la imagen desde el directorio del Dockerfile
-sudo docker build -t web1-cavaliere $dockerfile_path
+# Contenido que se debe agregar al archivo index.html:
+# <div>
+# <h1> Sistemas Operativos - UTNFRA </h1></br>
+# <h2> 2do Parcial - Noviembre 2024 </h2> </br>
+# <h3> Santiago Cavaliere</h3>
+# <h3> División: 311</h3>
+# </div>
+
+sudo vim Dockerfile
+
+# Contenido que se debe agregar al Dockerfile:
+# FROM nginx:latest
+#
+# COPY index.html /usr/share/nginx/html/index.html
+#
+# EXPOSE 80
+
+sudo docker build -t web1-cavaliere .
 
 sudo docker images
-# Agrego el usuario actual al grupo Docker porque sino luego voy a tener fallas
+
+# Acá agrego el usuario actual (santicavaliere) al grupo docker para evitar usar sudo constantemente
 sudo usermod -aG docker santicavaliere
 
-# Uso este comando para evitar salir de ssh
+# Uso este comando para evitar tener que cerrar la máquina
 newgrp docker
 
 sudo docker login -u santicavaliere
-dckr_pat_WVapcEDhmJ7EIWG2R-fugwknVeM
+# Acá va mi contraseña
 
 docker tag web1-cavaliere santicavaliere/web1:latest
 
@@ -32,14 +39,21 @@ docker images
 
 docker push santicavaliere/web1:latest
 
-cat <<EOF > run.sh
-#!/bin/bash
+# Acá se crea el archivo run.sh para ejecutar el contenedor
+sudo vim run.sh
 
-docker run -d -p 8080:80 santicavaliere/web1:latest
-EOF
+# Contenido que se debe agregar al archivo run.sh:
+# #!/bin/bash
+#
+# docker run -d -p 8080:80 santicavaliere/web1:latest
 
 sudo chmod +x run.sh
+
 sudo ./run.sh
+
+# Verificar que el contenedor está corriendo
 docker ps
+
+# Probé con curl porque vi que era más rápido, sin embargo, probando con el ip en una pestaña de windows también funciona
 curl http://localhost:8080
 
